713764e71267d5c475caa4794f317f420f38aba3
DanielRad082@gmail.com <Danielrad082@gmail.com>
2020-07-05 20:55:01 +0200

    Finnished makeroutes script

--- MakeRoutes.py
+++ MakeRoutes.py
@@ -15,25 +15,51 @@ while True:
     curSelect.execute("SELECT RouteID FROM RoutesTBL WHERE RouteID = ?",
                       (stdin,))
     result = curSelect.fetchone()
-    if len(result[0]) <= 1:
+    if result is None:
         print("Route not found. Aborting!!!")
         continue
-    intervali = 0
 
-    while True:
+    curSelect.execute("SELECT Interval FROM RouteStopsTBL WHERE RouteID = ?"
+                      " ORDER BY Interval DESC", (result[0],))
+    intervaltime = curSelect.fetchone()
+    if intervaltime is None:
+        intervali = 0
+    else:
+        intervali = intervaltime[0]
+    del(intervaltime)
+
+    curSelect.execute("SELECT RoutePosition FROM RouteStopsTBL WHERE RouteID"
+                      " = ? ORDER BY RoutePosition DESC", (result[0],))
+    POS = curSelect.fetchone()
+    if POS is None:
         routePosHID = 0
-        busStopName = input("Stop (" + routePosHID+1 + "); Enter Stop Name: ")
-        currEdit.execute("Select BusStopID From BusStopTBL " +
-                         "where Location = ?", (busStopName,))
+    else:
+        routePosHID = int(POS[0]) + 1
+    del(POS)
 
-        responce.fetchone()
+    curSelect.close()
+    while True:
+        busStopName = input("Stop (" + str(routePosHID+1) +
+                            "); Enter Stop Name: ")
+        if busStopName == "":
+            break
+        curEdit.execute("Select BusStopID From BusStopTBL " +
+                        "where Location = ?", (busStopName,))
+
+        responce = curEdit.fetchone()
         busStopID = responce[0]
+        if busStopID is None:
+            print("Invalid Name")
+            del(responce)
+            continue
         del(responce)
 
         while True:
-            interval = input("Select a interval, current(" + intervali + "): ")
+            interval = input("Select a interval (minutes), current(" +
+                             str(intervali) + "): ")
             try:
                 intervali = int(interval)
+                del(interval)
                 break
             except TypeError:
                 print("Error invalid number, please enter a integer (1,2,3,"
@@ -41,7 +67,9 @@ while True:
         try:
             curEdit.execute("INSERT INTO RouteStopsTBL (RouteID, RoutePosition"
                             ", BusStopID, Interval) VALUES(?,?,?,?)",
-                            (result[0], routePosHID-1,busStopID,intervali))
+                            (result[0], routePosHID, busStopID, intervali))
+            routePosHID += 1
+            print("Success")
+            conEdit.commit()
         except sqlite3.DatabaseError:
             print("Something when't wrong and we don't know what lol.")
-        routePosHID =+1
